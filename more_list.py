4eeb992d1f7dd691630b8f124d39a482fabd86db
Giangkhamliu <grace21@navgurukul.org>
2021-12-13 20:26:55 +0530

    y first commit

--- more_list.py
+++ more_list.py
@@ -1,13 +1,13 @@
 # 1.Take 10 integer inputs from user and store 
 # them in a list and print them on screen.
-# i = 10
-# a = []
-# while i>0:
-#   print("Enter number:")
-#   num = input()
-#   a.append(num)
-#   i = i-1
-# print(a)
+i = 10
+a = []
+while i>0:
+  print("Enter number:")
+  num = input()
+  a.append(num)
+  i = i-1
+print(a)
 
 # 2.Take 10 integer inputs from user and store them in a list. 
 # Again ask user to give a number. Now, tell user whether that
@@ -25,3 +25,4 @@ if n in a:
     print("Yes")
 else:
     print("No")
+
--- num.py
+++ num.py
@@ -1,5 +1,35 @@
-number=[2,4,5,6,7]
-i=0
-while i<len(number):
-    print(number[i])
-    i=i+1
+# number=[2,4,5,6,7]
+# i=0
+# while i<len(number):
+#     print(number[i])
+#     i=i+1
+i=5
+a=[]
+while i>0:
+  print("Enter the number:")
+  num=input()
+  a.append(num)
+  i-=1
+odd=0
+even=0
+positive=0
+negative=0
+zero=0
+i=19
+while i>=0:
+  if a[i]==0:
+    zero=zero+1
+  elif a[i]>0:
+    positive=positive+1
+    if a[i]%2==0:
+      even=even+1
+    else:
+      odd=odd+1
+  else:
+    negative=negative+1
+    if a[i]%2==0:
+      even=even+1
+    else:
+          odd=odd+1
+  i-=1
+print("EVEN:",even,"ODD:",even,"POSITIVE:",positive,"NEGATIVE:",negative,"ZERO",zero)
