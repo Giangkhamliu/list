82dcf39a19e4fda6a87d135e7a8813cea4164b26
Giangkhamliu <grace21@navgurukul.org>
2021-12-14 20:04:24 +0530

    y first commit

--- meraki_lists.py
+++ meraki_lists.py
@@ -1,48 +1,47 @@
-# # numbers=[50, 40, 23, 70, 56, 12, 5, 10, 7]
-# # print(len(numbers))
-
 # numbers=[50, 40, 23, 70, 56, 12, 5, 10, 7]
-# count=0
-# i=0
-# # while i<len(numbers):
-# for i in range(len(numbers)):
-#     if numbers[i]>=20 and numbers[i]<=40:
-#        count=count+numbers[i]
-#        print(numbers[i])
-#     i=i+1
+# print(len(numbers))
+
+numbers=[50, 40, 23, 70, 56, 12, 5, 10, 7]
+count=0
+i=0
+# while i<len(numbers):
+for i in range(len(numbers)):
+    if numbers[i]>=20 and numbers[i]<=40:
+       count=count+numbers[i]
+       print(numbers[i])
+    i=i+1
     
-# numbers = [50, 40, 23, 70, 56, 12, 5, 10, 7]
-# # max(numbers)
-# # print(max(numbers))
-# numbers.sort()
-# print(numbers)
-# print("The maximum no. is=",numbers[-1])
+numbers = [50, 40, 23, 70, 56, 12, 5, 10, 7]
+# max(numbers)
+# print(max(numbers))
+numbers.sort()
+print(numbers)
+print("The maximum no. is=",numbers[-1])
 
-# numbers = [50, 40, 23, 70, 56, 12, 5, 10, 7]
-# numbers.sort()
-# print("The maximum no. is=",numbers[-2])
+numbers = [50, 40, 23, 70, 56, 12, 5, 10, 7]
+numbers.sort()
+print("The maximum no. is=",numbers[-2])
 
-# places=["delhi", "gujarat", "rajasthan", "punjab", "kerala"]
-# # print(places[4])
-# # print(places[3])
-# # print(places[2])
-# # print(places[1])
-# # print(places[0])
-# i=1
-# while i<len(places)+1:
-#     print(places[-i])
-#     i=i+1
+places=["delhi", "gujarat", "rajasthan", "punjab", "kerala"]
+# print(places[4])
+# print(places[3])
+# print(places[2])
+# print(places[1])
+# print(places[0])
+i=1
+while i<len(places)+1:
+    print(places[-i])
+    i=i+1
 
-# # name=[ 'n', 'i', 't', 'i', 'n' ]
-# name=['g','r','a','c','e']
-# if name[0]==name[4]:
-#     print(name,"Palindrome")
-# else:
-#     print(name,"Not palindrome")
+# name=[ 'n', 'i', 't', 'i', 'n' ]
+name=['g','r','a','c','e']
+if name[0]==name[4]:
+    print(name,"Palindrome")
+else:
+    print(name,"Not palindrome")
     
-list1 = [1,2,3,4,5,6]
-list2 = [2,3,1,0,6,7]
-list3=list1-list2
-print(list3)
+
+
+
 
     
--- more_list.py
+++ more_list.py
@@ -1,30 +1,30 @@
-# # 1.Take 10 integer inputs from user and store 
-# # them in a list and print them on screen.
-# i = 10
-# a = []
-# while i>0:
-#   print("Enter number:")
-#   num = input()
-#   a.append(num)
-#   i = i-1
-# print(a)
+# 1.Take 10 integer inputs from user and store 
+# them in a list and print them on screen.
+i = 10
+a = []
+while i>0:
+  print("Enter number:")
+  num = input()
+  a.append(num)
+  i = i-1
+print(a)
 
-# # 2.Take 10 integer inputs from user and store them in a list. 
-# # Again ask user to give a number. Now, tell user whether that
-# #  number is present in list or not.
-# # (Iterate over list using while loop ).
-# i = 10
-# a = []
-# while i>0:
-#   print("Enter number:")
-#   num = input()
-#   a.append(num)
-#   i = i-1
-# n=input("ENTER ANY NUMBER")
-# if n in a:
-#     print("Yes")
-# else:
-#     print("No")
+# 2.Take 10 integer inputs from user and store them in a list. 
+# Again ask user to give a number. Now, tell user whether that
+#  number is present in list or not.
+# (Iterate over list using while loop ).
+i = 10
+a = []
+while i>0:
+  print("Enter number:")
+  num = input()
+  a.append(num)
+  i = i-1
+n=input("ENTER ANY NUMBER")
+if n in a:
+    print("Yes")
+else:
+    print("No")
 
 
 i=10
